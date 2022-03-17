512ef123c2b1a55cc5075ae8bff75ab8c65df962
Sok4h <mrsokaxd@gmail.com>
2022-03-17 12:50:49 -0500

    Login funcionando sin persistencia

---
+++ app/src/main/java/com/sokah/retoapps/CONSTANTS.kt
@@ -0,0 +1,5 @@
+package com.sokah.retoapps
+
+val USER1_EMAIL = "alfa@gmail.com"
+val USER2_EMAIL = "beta@gmail.com"
+val USERPASS ="aplicacionesmoviles"
--- app/src/main/java/com/sokah/retoapps/LoginActivity.kt
+++ app/src/main/java/com/sokah/retoapps/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.sokah.retoapps
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.sokah.retoapps.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -12,5 +15,45 @@ class LoginActivity : AppCompatActivity() {
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+
+            if(binding.inputEmail.editText?.text!!.toString().isNotEmpty()&&binding.inputPassword.editText?.text!!.toString().isNotEmpty()){
+
+                Login()
+            }
+
+            else{
+
+                Toast.makeText(this,"Por favor complete todos los campos",Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+
+    fun Login(){
+
+        val email = binding.inputEmail.editText?.text!!.toString()
+        val password = binding.inputPassword.editText?.text!!.toString()
+        if(email.contentEquals(USER1_EMAIL)|| email.contentEquals(USER2_EMAIL)){
+
+            if(password.contentEquals(USERPASS)){
+
+                var intent = Intent(this,MainActivity::class.java)
+
+                startActivity(intent)
+                finish()
+//                val preferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+//                val editor = preferences.edit()
+//                editor.apply {
+//
+//                    putString("USER_EMAIL",email)
+//                }
+            }
+
+        }
+
+
     }
 }
